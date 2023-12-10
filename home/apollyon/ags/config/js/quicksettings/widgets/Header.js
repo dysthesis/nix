454fa37bd4eb096db69b4bdd3c7e399ca687a944
Dysthesis <antheoraviel@protonmail.com>
2023-12-11 08:27:31 +1100

    Fix the system status monitor and get rid of unnecessary components in AGS

--- home/apollyon/ags/config/js/quicksettings/widgets/Header.js
+++ home/apollyon/ags/config/js/quicksettings/widgets/Header.js
@@ -11,72 +11,65 @@ import { execAsync } from 'resource:///com/github/Aylur/ags/utils.js';
 
 
 export const BatteryProgress = () => Widget.Box({
-    class_name: 'battery-progress',
+  class_name: 'battery-progress',
+  vexpand: true,
+  binds: [['visible', Battery, 'available']],
+  connections: [[Battery, w => {
+    w.toggleClassName('charging', Battery.charging || Battery.charged);
+    w.toggleClassName('medium', Battery.percent < options.battery.medium.value);
+    w.toggleClassName('low', Battery.percent < options.battery.low.value);
+    w.toggleClassName('half', Battery.percent < 48);
+  }]],
+  child: Widget.Overlay({
     vexpand: true,
-    binds: [['visible', Battery, 'available']],
-    connections: [[Battery, w => {
-        w.toggleClassName('charging', Battery.charging || Battery.charged);
-        w.toggleClassName('medium', Battery.percent < options.battery.medium.value);
-        w.toggleClassName('low', Battery.percent < options.battery.low.value);
-        w.toggleClassName('half', Battery.percent < 48);
-    }]],
-    child: Widget.Overlay({
-        vexpand: true,
-        child: Widget.ProgressBar({
-            hexpand: true,
-            vexpand: true,
-            connections: [[Battery, progress => {
-                progress.fraction = Battery.percent / 100;
-            }]],
-        }),
-        overlays: [Widget.Label({
-            connections: [[Battery, l => {
-                l.label = Battery.charging || Battery.charged
-                    ? icons.battery.charging
-                    : `${Battery.percent}%`;
-            }]],
-        })],
+    child: Widget.ProgressBar({
+      hexpand: true,
+      vexpand: true,
+      connections: [[Battery, progress => {
+        progress.fraction = Battery.percent / 100;
+      }]],
     }),
+    overlays: [Widget.Label({
+      connections: [[Battery, l => {
+        l.label = Battery.charging || Battery.charged
+          ? icons.battery.charging
+          : `${Battery.percent}%`;
+      }]],
+    })],
+  }),
 });
 
 export default () => Widget.Box({
-    class_name: 'header horizontal',
-    children: [
-        Avatar(),
+  class_name: 'header horizontal',
+  children: [
+    Widget.Box({
+      class_name: 'system-box',
+      vertical: true,
+      hexpand: true,
+      children: [
         Widget.Box({
-            class_name: 'system-box',
-            vertical: true,
-            hexpand: true,
-            children: [
-                Widget.Box({
-                    children: [
-                        Widget.Button({
-                            vpack: 'center',
-                            on_clicked: openSettings,
-                            child: Widget.Icon(icons.ui.settings),
-                        }),
-                        Widget.Label({
-                            class_name: 'uptime',
-                            hexpand: true,
-                            vpack: 'center',
-                            connections: [[uptime, label => {
-                                label.label = `uptime: ${uptime.value}`;
-                            }]],
-                        }),
-                        Widget.Button({
-                            vpack: 'center',
-                            on_clicked: () => execAsync('gtklock'),
-                            child: Widget.Icon(icons.lock),
-                        }),
-                        Widget.Button({
-                            vpack: 'center',
-                            on_clicked: () => PowerMenu.action('shutdown'),
-                            child: Widget.Icon(icons.powermenu.shutdown),
-                        }),
-                    ],
-                }),
-                BatteryProgress(),
-            ],
+          children: [
+            Widget.Label({
+              class_name: 'uptime',
+              hexpand: true,
+              vpack: 'center',
+              connections: [[uptime, label => {
+                label.label = `Uptime: ${uptime.value}`;
+              }]],
+            }),
+            Widget.Button({
+              vpack: 'center',
+              on_clicked: () => execAsync('gtklock'),
+              child: Widget.Icon(icons.lock),
+            }),
+            Widget.Button({
+              vpack: 'center',
+              on_clicked: () => PowerMenu.action('shutdown'),
+              child: Widget.Icon(icons.powermenu.shutdown),
+            }),
+          ],
         }),
-    ],
+      ],
+    }),
+  ],
 });
