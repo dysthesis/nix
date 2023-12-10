0ec71e153f4c137293f162b64e340c75a56917a8
Dysthesis <antheoraviel@protonmail.com>
2023-12-11 08:30:23 +1100

    Don't touch my wallpapers please, AGS

--- home/apollyon/ags/config/js/settings/setup.js
+++ home/apollyon/ags/config/js/settings/setup.js
@@ -13,125 +13,123 @@ import { exec, execAsync } from 'resource:///com/github/Aylur/ags/utils.js';
 
 
 export function init() {
-    // initWallpaper();
-    notificationBlacklist();
-    warnOnLowBattery();
-    globals();
-    tmux();
-    gsettigsColorScheme();
-    gtkFontSettings();
-    scssWatcher();
+  // initWallpaper();
+  notificationBlacklist();
+  warnOnLowBattery();
+  globals();
+  tmux();
+  gsettigsColorScheme();
+  gtkFontSettings();
+  scssWatcher();
 
-    App.connect('config-parsed', () => {
-        reloadScss();
-        // hyprlandInit();
-        wallpaper();
-        pywal();
-        gtkTheme();
-        gtkIcons();
-    });
+  App.connect('config-parsed', () => {
+    reloadScss();
+    pywal();
+    gtkTheme();
+    gtkIcons();
+  });
 }
 
 
 
 function tmux() {
-    if (!Utils.exec('which tmux'))
-        return;
+  if (!Utils.exec('which tmux'))
+    return;
 
-    /** @param {string} scss */
-    function getColor(scss) {
-        if (scss.includes('#'))
-            return scss;
+  /** @param {string} scss */
+  function getColor(scss) {
+    if (scss.includes('#'))
+      return scss;
 
-        if (scss.includes('$')) {
-            const opt = options.list().find(opt => opt.scss === scss.replace('$', ''));
-            return opt?.value;
-        }
+    if (scss.includes('$')) {
+      const opt = options.list().find(opt => opt.scss === scss.replace('$', ''));
+      return opt?.value;
     }
+  }
 
-    options.theme.accent.accent.connect('changed', ({ value }) => Utils
-        .execAsync(`tmux set @main_accent ${getColor(value)}`)
-        .catch(err => console.error(err.message)));
+  options.theme.accent.accent.connect('changed', ({ value }) => Utils
+    .execAsync(`tmux set @main_accent ${getColor(value)}`)
+    .catch(err => console.error(err.message)));
 }
 
 function gsettigsColorScheme() {
-    if (!Utils.exec('which gsettings'))
-        return;
+  if (!Utils.exec('which gsettings'))
+    return;
 
-    options.theme.scheme.connect('changed', ({ value }) => {
-        const gsettings = 'gsettings set org.gnome.desktop.interface color-scheme';
-        Utils.execAsync(`${gsettings} "prefer-${value}"`)
-            .catch(err => console.error(err.message));
-    });
+  options.theme.scheme.connect('changed', ({ value }) => {
+    const gsettings = 'gsettings set org.gnome.desktop.interface color-scheme';
+    Utils.execAsync(`${gsettings} "prefer-${value}"`)
+      .catch(err => console.error(err.message));
+  });
 }
 
 function gtkFontSettings() {
-    const settings = Gtk.Settings.get_default();
-    if (!settings) {
-        console.error(Error('Gtk.Settings unavailable'));
-        return;
-    }
+  const settings = Gtk.Settings.get_default();
+  if (!settings) {
+    console.error(Error('Gtk.Settings unavailable'));
+    return;
+  }
 
-    const callback = () => {
-        const { size, font } = options.font;
-        settings.gtk_font_name = `${font.value} ${size.value}`;
-    };
+  const callback = () => {
+    const { size, font } = options.font;
+    settings.gtk_font_name = `${font.value} ${size.value}`;
+  };
 
-    options.font.font.connect('notify::value', callback);
-    options.font.size.connect('notify::value', callback);
+  options.font.font.connect('notify::value', callback);
+  options.font.size.connect('notify::value', callback);
 }
 
 function notificationBlacklist() {
-    Notifications.connect('notified', (_, id) => {
-        const n = Notifications.getNotification(id);
-        options.notifications.black_list.value.forEach(item => {
-            if (n?.app_name.includes(item) || n?.app_entry?.includes(item))
-                n.close();
-        });
+  Notifications.connect('notified', (_, id) => {
+    const n = Notifications.getNotification(id);
+    options.notifications.black_list.value.forEach(item => {
+      if (n?.app_name.includes(item) || n?.app_entry?.includes(item))
+        n.close();
     });
+  });
 }
 
 function warnOnLowBattery() {
-    Battery.connect('notify::percent', () => {
-        const low = options.battery.low.value;
-        if (Battery.percent !== low ||
-            Battery.percent !== low / 2 ||
-            !Battery.charging)
-            return;
+  Battery.connect('notify::percent', () => {
+    const low = options.battery.low.value;
+    if (Battery.percent !== low ||
+      Battery.percent !== low / 2 ||
+      !Battery.charging)
+      return;
 
-        Utils.execAsync([
-            'notify-send',
-            `${Battery.percent}% Battery Percentage`,
-            '-i', icons.battery.warning,
-            '-u', 'critical',
-        ]);
-    });
+    Utils.execAsync([
+      'notify-send',
+      `${Battery.percent}% Battery Percentage`,
+      '-i', icons.battery.warning,
+      '-u', 'critical',
+    ]);
+  });
 }
 
 export function pywal() {
-    if (!exec('which wal'))
-        return print('missing dependancy: pywal');
+  if (!exec('which wal'))
+    return print('missing dependancy: pywal');
 
-    execAsync([
-        'wal', '--theme', options.misc.pywal.theme.value,
-    ]).catch(err => console.error(err));
+  execAsync([
+    'wal', '--theme', options.misc.pywal.theme.value,
+  ]).catch(err => console.error(err));
 }
 
 export function gtkTheme() {
-    if (!exec('which gsettings'))
-        return print('missing dependancy: Gnome-Settings');
+  if (!exec('which gsettings'))
+    return print('missing dependancy: Gnome-Settings');
 
-    execAsync([
-        'gsettings', 'set', 'org.gnome.desktop.interface', 'gtk-theme', options.misc.gtk.theme.value,
-    ]).catch(err => console.error(err));
+  execAsync([
+    'gsettings', 'set', 'org.gnome.desktop.interface', 'gtk-theme', options.misc.gtk.theme.value,
+  ]).catch(err => console.error(err));
 }
 
 
 export function gtkIcons() {
-    if (!exec('which gsettings'))
-        return print('missing dependancy: Gnome-Settings');
+  if (!exec('which gsettings'))
+    return print('missing dependancy: Gnome-Settings');
 
-    execAsync([
-        'gsettings', 'set', 'org.gnome.desktop.interface', 'icon-theme', options.misc.gtkIcons.theme.value,
-    ]).catch(err => console.error(err));
+  execAsync([
+    'gsettings', 'set', 'org.gnome.desktop.interface', 'icon-theme', options.misc.gtkIcons.theme.value,
+  ]).catch(err => console.error(err));
 }
